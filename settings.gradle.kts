85c3153e94b99bdc9844609f93d4b5ea1887701b
debrajghosh <dbrj.pprof@gmail.com>
2026-06-29 17:45:07 +0200

    First commit initialization

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "nextxi-backend"
---
+++ src/test/kotlin/com/example/nextxibackend/NextxiBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.nextxibackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NextxiBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
