d9126e82c45d33bfeb6ee96b9259535809773e20
AvikRaxit <avik.prof@gmail.com>
2026-08-15 03:24:16 +0530

    Update TeamController by removing unused import

--- src/main/kotlin/com/debrajghosh/nextxi/controller/TeamController.kt
+++ src/main/kotlin/com/debrajghosh/nextxi/controller/TeamController.kt
@@ -1,16 +1,10 @@
 package com.debrajghosh.nextxi.controller
 
 import com.debrajghosh.nextxi.dto.TeamDTO
-import com.debrajghosh.nextxi.entity.Team
 import com.debrajghosh.nextxi.service.TeamService
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
